63a8a915260cf45af53a242c0e68899c1b3e3400
Maker Zone <dev@makerzone.com.au>
2024-09-20 17:38:46 +1000

    Refactored prepare-libraries.sh to use function-based cloning for libraries

--- prepare-libraries.sh
+++ prepare-libraries.sh
@@ -1,37 +1,25 @@
 #!/bin/bash
 
-# Define the Arduino libraries folder
-LIBRARY_PATH1="$HOME/Arduino/libraries/DisplayValueCommon"
+# Function to clone libraries if not already present
+install_library() {
+  local LIBRARY_NAME="$1"
+  local LIBRARY_PATH="$HOME/Arduino/libraries/$LIBRARY_NAME"
+  local GIT_URL="$2"
 
-# Check if DisplayValueCommon is already present
-if [ ! -d "$LIBRARY_PATH1" ]; then
-    echo "Cloning DisplayValueCommon library into Arduino libraries folder..."
-    git clone https://github.com/makers-multiverse/DisplayValueCommon.git "$LIBRARY_PATH1"
-else
-    echo "DisplayValueCommon library already exists, skipping clone."
-fi
+  if [ ! -d "$LIBRARY_PATH" ]; then
+    echo "Cloning $LIBRARY_NAME into Arduino libraries folder..."
+    git clone "$GIT_URL" "$LIBRARY_PATH"
+  else
+    echo "$LIBRARY_NAME already exists, skipping clone."
+  fi
+}
 
-# Define the Arduino libraries folder for LCDI2C
-LIBRARY_PATH2="$HOME/Arduino/libraries/LCDI2C"
+# Install required libraries
+install_library "DisplayValueCommon" "https://github.com/makers-multiverse/DisplayValueCommon.git"
+install_library "LCDI2C" "https://github.com/makers-multiverse/LCDI2C.git"
+install_library "SerialLogger" "https://github.com/makers-multiverse/SerialLogger.git"
 
-# Check if LCDI2C is already present
-if [ ! -d "$LIBRARY_PATH2" ]; then
-    echo "Cloning LCDI2C library into Arduino libraries folder..."
-    git clone https://github.com/makers-multiverse/LCDI2C.git "$LIBRARY_PATH2"
-else
-    echo "LCDI2C library already exists, skipping clone."
-fi
-
-# Define the Arduino libraries folder for SerialLogger
-LIBRARY_PATH3="$HOME/Arduino/libraries/SerialLogger"
-
-# Check if SerialLogger is already present
-if [ ! -d "$LIBRARY_PATH3" ]; then
-    echo "Cloning SerialLogger library into Arduino libraries folder..."
-    git clone https://github.com/makers-multiverse/SerialLogger.git "$LIBRARY_PATH3"
-else
-    echo "SerialLogger library already exists, skipping clone."
-fi
+# Finish
 
 
 echo "Library preparation complete."

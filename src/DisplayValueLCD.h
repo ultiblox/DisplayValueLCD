90ffc5347b557fd41ab953c87a4c7b906d66a8e9
Maker Zone <dev@makerzone.com.au>
2024-11-06 12:34:39 +1100

    Removing dependence on DisplayValueCommon

--- src/DisplayValueLCD.h
+++ src/DisplayValueLCD.h
@@ -1,28 +1,25 @@
 #ifndef DisplayValueLCD_h
 #define DisplayValueLCD_h
 
-#include "DisplayValueCommon.h"
 #include <LCDI2C.h>
 
-class DisplayValueLCD : public DisplayValueCommon {
+class DisplayValueLCD {
 private:
     LCDI2C lcd;
     uint16_t numCols;
     char lastLabel[32];   // Fixed-size buffer for the label
     char lastValue[32];   // Fixed-size buffer for the last displayed value
-    char suffix[8];       // Increased buffer size for the suffix
+    char suffix[8];       // Buffer size for the suffix
 
 public:
     DisplayValueLCD(uint8_t address, uint16_t cols, uint16_t rows);
-    void init() override;
-    void clear() override;
-    void setLabel(const char* value) override;
-    void setLabel(const __FlashStringHelper* value) override;
-    
-    void setValue(const char* value) override;
-    void setValue(int value) override;
-    
-    void setSuffix(const char* value) override;
+    void init();
+    void clear();
+    void setLabel(const char* value);
+    void setLabel(const __FlashStringHelper* value);
+    void setValue(const char* value);
+    void setValue(int value);
+    void setSuffix(const char* value);
     int calculateStartPosition(const char* value);
 };
 
